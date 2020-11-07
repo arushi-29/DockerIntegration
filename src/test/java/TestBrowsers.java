3ea7303577d70aab1999da485528abd079d9daea
arushi-29 <arushichauhan29@yahoo.com>
2020-11-07 19:48:53 +0000

    First commit

--- src/test/java/TestBrowsers.java
+++ src/test/java/TestBrowsers.java
@@ -1,29 +1,48 @@
-import org.testng.annotations.Test;
-import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.Test;
-import java.util.List;
+import java.net.URL;
 
-import javax.swing.text.html.HTMLDocument.Iterator;
-
-import org.openqa.selenium.*;
-import org.openqa.selenium.chrome.*;
-import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
 
 public class TestBrowsers {
 
 	public static WebDriver driver;
 	public static WebDriverWait wait;
+	String testurl = "https://google.com";
 
-	@BeforeClass(alwaysRun = true)
-	public static void FirstRun() throws Exception {
-		System.setProperty("webdriver.chrome.driver", "C:\\chromedriver\\chromedriver.exe");
-		driver = new ChromeDriver();
-		driver.get("https://google.com");
+	@Parameters({"browser"})
+	@BeforeClass
+	public static void FirstRun(String Browser) throws Exception 
+	{
+		if(Browser.equalsIgnoreCase("chrome"))
+		{
+			ChromeOptions options = new ChromeOptions();
+			options.addArguments("--start-maximized");
+			options.setCapability(ChromeOptions.CAPABILITY, options);
+			options.setCapability("browserName", "chrome");
+			options.setCapability("acceptSslCerts", "true");
+			options.setCapability("javascriptEnabled", "true");		
+			DesiredCapabilities cap = DesiredCapabilities.chrome();
+			options.merge(cap);
+			try
+			{
+				driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), options);
+			}
+			catch(Exception e)
+			{
+				e.printStackTrace();
+			}
+		}
+		
 	}
 
 	@Test(priority = 0)
@@ -31,6 +50,8 @@ public class TestBrowsers {
 
 		wait = new WebDriverWait(driver, 40);
 		driver.findElement(By.name("q")).sendKeys("Selenium");
+		driver.findElement(By.className("Tg7LZd")).sendKeys(Keys.ENTER);
+		
 	}
 
 }
